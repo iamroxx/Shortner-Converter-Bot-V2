fd158c79b58f90c19f8ad25be16c11557a52aa0f
iamroxx <98517644+iamroxx@users.noreply.github.com>
2023-07-06 17:05:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/Cyniteofficial/Short.gi t /Shortner-Converter-Bot-V2
+git clone https://github.com/Cyniteofficial/Short.git/Short
 cd /Short
 pip3 install -r requirements.txt
 echo "Starting Bot...."
